4c5e8069aeccc065c1ccd7191659b460cd76722c
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-06-05 18:35:38 -0300

    feat: start project

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :delivery,
+  ecto_repos: [Delivery.Repo]
+
+# Configures the endpoint
+config :delivery, DeliveryWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "J10Wk6Rhg3H1bXYJkLa6cGN9fFx2RiSCNRA7BNDGaOZAaJsXLnDRYlaNhh1ndtc8",
+  render_errors: [view: DeliveryWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: Delivery.PubSub,
+  live_view: [signing_salt: "0KoPgnQR"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
