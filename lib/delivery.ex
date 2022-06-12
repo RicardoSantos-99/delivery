b1298809ed940b577ea233e0c36290749599ed56
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-06-12 20:19:24 -0300

    feat: add delete user

--- lib/delivery.ex
+++ lib/delivery.ex
@@ -1,7 +1,9 @@
 defmodule Delivery do
   alias Delivery.Users.Create, as: UserCreate
   alias Delivery.Users.Get, as: UserGet
+  alias Delivery.Users.Delete, as: UserDelete
 
   defdelegate create_user(params), to: UserCreate, as: :call
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
+  defdelegate delete_user(id), to: UserDelete, as: :call
 end
--- lib/delivery_web/controllers/users_controller.ex
+++ lib/delivery_web/controllers/users_controller.ex
@@ -14,6 +14,14 @@ defmodule DeliveryWeb.UsersController do
     end
   end
 
+  def delete(conn, %{"id" => id}) do
+    with {:ok, %User{}} <- Delivery.delete_user(id) do
+      conn
+      |> put_status(:no_content)
+      |> text("")
+    end
+  end
+
   def show(conn, %{"id" => id}) do
     with {:ok, %User{} = user} <- Delivery.get_user_by_id(id) do
       conn
