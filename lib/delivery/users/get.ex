7bba87bdda77353fe9c9aba0b7c3c05c618a3730
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-06-12 17:47:01 -0300

    feat: validation in by_id

--- lib/delivery/users/get.ex
+++ lib/delivery/users/get.ex
@@ -1,7 +1,15 @@
 defmodule Delivery.Users.Get do
+  alias Ecto.UUID
   alias Delivery.{Repo, User}
 
   def by_id(id) do
+    case UUID.cast(id) do
+      :error -> {:error, %{status: :bad_request, result: %{id: "Invalid UUID"}}}
+      {:ok, uuid} -> get(uuid)
+    end
+  end
+
+  defp get(id) do
     case Repo.get(User, id) do
       nil -> {:error, %{status: :not_found, result: "User not found"}}
       user -> {:ok, user}
