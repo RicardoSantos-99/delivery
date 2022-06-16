55662f79d1c4c96ff496315de44b6ac7fa6a9722
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-06-16 11:55:56 -0300

    feat: create item table

--- priv/repo/migrations/20220616144412_create_item_category_type.exs
+++ priv/repo/migrations/20220616144412_create_item_category_type.exs
@@ -2,8 +2,8 @@ defmodule Delivery.Repo.Migrations.CreateItemCategoryType do
   use Ecto.Migration
 
   def change do
-    up_query = "CREATE TYPE item_category_type AS ENUM ('food', 'drink', 'desert')"
-    down_query = "DROP TYPE item_category_type"
+    up_query = "CREATE TYPE item_category AS ENUM ('food', 'drink', 'desert')"
+    down_query = "DROP TYPE item_category"
 
     execute(up_query, down_query)
   end
