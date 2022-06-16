7310db38838e5f9ec54a9ce500e6126cba5af10a
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-06-16 17:15:10 -0300

    chore: add validation items

--- lib/delivery/orders/create.ex
+++ lib/delivery/orders/create.ex
@@ -12,7 +12,17 @@ defmodule Delivery.Orders.Create do
 
     query = from item in Item, where: item.id in ^items_ids
 
-    Repo.all(query)
+    query
+    |> Repo.all()
+    |> validade_items(items_ids)
+  end
+
+  defp validade_items(items, items_ids) do
+    items_map = Map.new(items, fn item -> {item.id, item} end)
+
+    items_ids
+    |> Enum.map(fn id -> {id, Map.get(items_map, id)} end)
+    |> Enum.any?(fn {_id, value} -> is_nil(value) end)
   end
 end
 
