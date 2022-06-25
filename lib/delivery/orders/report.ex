060cbdfde37d96b3a609c96249121fccd8de6314
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-06-25 17:41:54 -0300

    feat: add streams in reports

--- lib/delivery/orders/report.ex
+++ lib/delivery/orders/report.ex
@@ -2,14 +2,17 @@ defmodule Delivery.Orders.Report do
   import Ecto.Query
   alias Delivery.{Repo, Order}
 
+  @default_batch_size 500
+
   def create(_filename \\ "report.csv") do
     query = from order in Order, order_by: order
 
     Repo.transaction(fn ->
       query
-      |> Repo.stream()
+      |> Repo.stream(max_rows: @default_batch_size)
+      |> Stream.chunk_every(@default_batch_size)
+      |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
       |> Enum.into([])
-      |> IO.inspect()
     end)
   end
 end
