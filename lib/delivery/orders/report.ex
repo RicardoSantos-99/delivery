e164b846a67ee280b02258a3af1c6f113709b071
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-06-26 14:12:31 -0300

    feat: add total prices

--- lib/delivery/orders/report.ex
+++ lib/delivery/orders/report.ex
@@ -2,24 +2,34 @@ defmodule Delivery.Orders.Report do
   import Ecto.Query
   alias Delivery.{Repo, Order, Item}
 
+  alias Delivery.Orders.TotalPrice
+
   @default_batch_size 500
 
-  def create(_filename \\ "report.csv") do
+  def create(filename \\ "report.csv") do
     query = from order in Order, order_by: order
 
-    Repo.transaction(fn ->
-      query
-      |> Repo.stream(max_rows: @default_batch_size)
-      |> Stream.chunk_every(@default_batch_size)
-      |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
-      |> Enum.map(&parse_line/1)
-    end)
+    {:ok, order_list} =
+      Repo.transaction(
+        fn ->
+          query
+          |> Repo.stream(max_rows: @default_batch_size)
+          |> Stream.chunk_every(@default_batch_size)
+          |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
+          |> Enum.map(&parse_line/1)
+        end,
+        timeout: :infinity
+      )
+
+    File.write(filename, order_list)
   end
 
   defp parse_line(%Order{user_id: user_id, payment_method: payment_method, items: items}) do
+    total_price = TotalPrice.calculate(items)
+
     items_string = Enum.map(items, &item_string/1)
 
-    "#{user_id},#{payment_method},#{items_string}#{50.0}"
+    "#{user_id},#{payment_method},#{items_string}#{total_price}\n"
   end
 
   defp item_string(%Item{category: category, description: description, price: price}) do
