ad0c1b740a2f835d211b24d3aebf1266c6d2dd17
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-06-26 18:13:20 -0300

    feat: add client for stack

--- lib/delivery/stack.ex
+++ lib/delivery/stack.ex
@@ -1,6 +1,23 @@
 defmodule Delivery.Stack do
   use GenServer
 
+  # CLIENT
+  def start_link(initial_stack) when is_list(initial_stack) do
+    GenServer.start_link(__MODULE__, initial_stack)
+  end
+
+  def push(pid, element) do
+    GenServer.cast(pid, {:push, element})
+  end
+
+  def pop(pid) do
+    GenServer.call(pid, :pop)
+  end
+
+  def show(pid) do
+    GenServer.call(pid, :show)
+  end
+
   @impl true
   def init(stack) do
     {:ok, stack}
@@ -21,6 +38,11 @@ defmodule Delivery.Stack do
   @impl true
   def handle_call(:pop, _from, []) do
     {:reply, nil, []}
+  end
+
+  @impl true
+  def handle_call(:show, _from, stack) do
+    {:reply, stack, stack}
   end
 
   # ASYNC
