43764125e00a1cd8b8e3b2e9d4aef06cfbe89634
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-06-26 12:45:59 -0300

    feat: add item string in report

--- lib/delivery/orders/report.ex
+++ lib/delivery/orders/report.ex
@@ -1,6 +1,6 @@
 defmodule Delivery.Orders.Report do
   import Ecto.Query
-  alias Delivery.{Repo, Order}
+  alias Delivery.{Repo, Order, Item}
 
   @default_batch_size 500
 
@@ -12,7 +12,17 @@ defmodule Delivery.Orders.Report do
       |> Repo.stream(max_rows: @default_batch_size)
       |> Stream.chunk_every(@default_batch_size)
       |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
-      |> Enum.into([])
+      |> Enum.map(&parse_line/1)
     end)
+  end
+
+  defp parse_line(%Order{user_id: user_id, payment_method: payment_method, items: items}) do
+    items_string = Enum.map(items, &item_string/1)
+
+    "#{user_id},#{payment_method},#{items_string}#{50.0}"
+  end
+
+  defp item_string(%Item{category: category, description: description, price: price}) do
+    "#{category},#{description},#{price},"
   end
 end
